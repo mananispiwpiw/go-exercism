aea6176c47785092a53311eb07f02a767df2b639
mananispiwpiw <rafijaeger@gmail.com>
2023-08-11 16:25:52 +0700

    solved: logs problem

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,54 @@
 package logs
 
+import "unicode/utf8"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	recommendation := '❗'
+	search := '🔍'
+	weather := '☀'
+	var result string
+
+	for _, value := range log {
+		if value == recommendation {
+			result = "recommendation"
+			break
+		} else if value == search {
+			result = "search"
+			break
+		} else if value == weather {
+			result = "weather"
+			break
+		} else {
+			result = "default"
+		}
+	}
+	return result
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	var result string
+	for _, value := range log {
+		if value == oldRune {
+			value = newRune
+			result += string(value)
+		} else {
+			result += string(value)
+		}
+	}
+	return result
 }
 
 // WithinLimit determines whether or not the number of characters in log is
-// within the limit.
+// within the limit
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	var result bool
+	if limit < utf8.RuneCountInString(log) {
+		result = false
+	} else if limit >= utf8.RuneCountInString(log) {
+		result = true
+	}
+	return result
 }
