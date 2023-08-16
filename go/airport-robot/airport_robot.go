3ea695d660a631f9d0f1f77f177001c64c43b26f
mananispiwpiw <rafijaeger@gmail.com>
2023-08-16 14:40:16 +0700

    solved: airport_robot problem

--- go/airport-robot/airport_robot.go
+++ go/airport-robot/airport_robot.go
@@ -1,5 +1,39 @@
 package airportrobot
 
 // Write your code here.
+type Greeter interface {
+	LanguageName() string
+	Greet(a string) string
+}
+
+// SayHello() function
+func SayHello(name string, g Greeter) string {
+	return "I can speak " + g.LanguageName() + ": " + g.Greet(name)
+}
+
+// Italian
+type Italian struct{}
+
+// Method Italian
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+func (i Italian) Greet(name string) string {
+	//"Ciao {name}!"
+	return "Ciao " + name + "!"
+}
+
+// Portugese
+type Portuguese struct{}
+
+// Method Portugese
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+func (p Portuguese) Greet(name string) string {
+	//" {name}!"
+	return "Olá " + name + "!"
+}
+
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
